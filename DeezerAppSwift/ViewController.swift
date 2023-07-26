98f9e7de5d56cdad98b59cea0ecbb90ec04ef5d3
mehmetfurkansakiz <mehmetfurkansakiz@gmail.com>
2023-07-27 01:18:42 +0300

    Added genreModel, genreViewModel, APIManager for create genreCollection view

---
+++ DeezerAppSwift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DeezerAppSwift
+//
+//  Created by furkan sakız on 27.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
